f850f9edb2ec547f07029bb89d22477db1a190cc
Xavi Clotet <xavi.clotet@credimarket.com>
2020-04-11 19:18:44 +0200

    examples using lodash-es

--- apps/test/src/app/app.module.ts
+++ apps/test/src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MylibModule } from '@xavitest/mylib';
+import { NgZorroAntdModule, NZ_I18N, es_ES } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+
+registerLocaleData(es);
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    MylibModule
+    MylibModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- libs/mylib/src/lib/mylib/mylib.component.ts
+++ libs/mylib/src/lib/mylib/mylib.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { take, orderBy, map } from 'lodash-es';
+
+const players = [
+  { player: 'Aleksandar Mitrovic', goals: 10, shots: 118 },
+  { player: 'Mohamed Salah', goals: 19, shots: 116 },
+  { player: 'Harry Kane', goals: 17, shots: 110 },
+  { player: 'Sergio Agüero', goals: 19, shots: 99 },
+  { player: 'Raùl Jiménez', goals: 12, shots: 96 },
+  { player: 'Paul Pogba', goals: 13, shots: 93 },
+  { player: 'Eden Hazard', goals: 16, shots: 86 },
+  { player: 'Pierre-Emerick Aubameyang', goals: 18, shots: 81 },
+  { player: 'Gylfi Sigurdsson', goals: 12, shots: 78 },
+  { player: 'Sadio Manè', goals: 18, shots: 77 },
+];
 
 @Component({
   selector: 'xavitest-mylib',
@@ -6,10 +20,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mylib.component.css']
 })
 export class MylibComponent implements OnInit {
-
+  sorted: any[];
+  top3: any[];
+  result: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.sorted = orderBy(players, ['goals', 'shots'], ['desc', 'asc']);
+    this.top3= take(this.sorted, 3);
+    this.result = map(this.top3, 'player');
   }
 
 }
